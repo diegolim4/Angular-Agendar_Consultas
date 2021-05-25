2b373f61371cfd859290f8b107b5f88a00b154bb
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-25 07:41:09 -0300

    Adicionado o componetes para autentificação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,12 +9,22 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule  } from '@angular/material/icon';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { LoginComponent } from './account/login/login.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { AccountComponent } from './account/shared/account/account.component';
+import { AuthComponent } from './account/shared/auth/auth.component';
+import { HomeComponent } from './components/template/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    AccountComponent,
+    AuthComponent,
+    HomeComponent,    
     
   ],
   imports: [
@@ -22,7 +32,8 @@ import { FooterComponent } from './components/template/footer/footer.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
