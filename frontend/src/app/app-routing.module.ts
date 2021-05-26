0720409394dbff2ceae4ecc6b2ab6a407d787381
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-26 09:47:14 -0300

    Adicionando rotas e o AuthGuard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { LoginComponent } from './account/login/login.component';
+import { AuthGuard } from './account/shared/auth.guard';
+import { HomeComponent } from './components/template/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  canActivate: [AuthGuard] 
+},
+  
+{
+  path:'',
+  component: LoginComponent,
+},
+
+{
+  path:'create-account',
+  component: CreateAccountComponent,
+}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],  
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
