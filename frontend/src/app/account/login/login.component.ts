24d1263e9c006ce87384d0b0f2f9502eeaa7e161
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-26 08:56:27 -0300

    Criado tela de login

--- frontend/src/app/account/login/login.component.ts
+++ frontend/src/app/account/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../shared/account.service';
+import { Cliente } from './../../clientes/shared/cliente';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  cliente!: Cliente;
+  
+  login = {
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.cliente = new Cliente();
   }
 
+  async onSubmit() {
+    try{
+      const result = await this.accountService.login(this.login);
+      console.log(`Login efetuado: ${result}`);
+      // aqui vai me levar para a rota vazia novamente
+      this.router.navigate(['']);
+    
+    }catch(err){
+      console.error(err);
+    }    
+  } 
 }
