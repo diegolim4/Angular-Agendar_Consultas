ddd022bebd7c4ae38ba238576191d747fb8f0d04
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-27 16:04:11 -0300

    Criada a rota de autentificação

--- nodeapi/src/controllers/authController.js
+++ nodeapi/src/controllers/authController.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
 
 const router = express.Router();
 
+// rotar de registro
 router.post('/register', async (req, res) => {
     
     const { email } = req.body;
@@ -22,6 +24,25 @@ router.post('/register', async (req, res) => {
     }catch(err){
         return res.status(400).send({error: 'Falha ao Registrar'});
     }
+});
+
+// rota de autentificação
+router.post('/authenticate', async (req, res,)=> {
+    
+    const{ email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user)
+        return res.status(400).send({error: 'Usuário não encontrado'});
+    
+    if(!await bcrypt.compare(password, user.password)) //compara se a senha do login é a mesma salva no banco de dados
+        return res.status(400).send({error: 'Senha Inválida'})
+
+    user.password = undefined;
+        
+    res.send({ user })   
+    
 });
 
 module.exports = app => app.use('/auth', router)
