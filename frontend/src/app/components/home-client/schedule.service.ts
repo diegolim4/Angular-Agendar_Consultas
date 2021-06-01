296273509ea31aed4166d7f212136fc6d168e51a
Suelen Almeida <lima.j.diego@gmail.com>
2021-06-01 12:16:53 -0300

    Importando e usando o snack bar module

--- frontend/src/app/components/home-client/schedule.service.ts
+++ frontend/src/app/components/home-client/schedule.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewScheduleService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  showMsg(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration:2000,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+      
+    })
+  }
 }
