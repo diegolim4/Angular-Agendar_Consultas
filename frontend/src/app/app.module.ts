26311dfa01ce41b0b93ed7b6e630fb71a1086230
Suelen Almeida <lima.j.diego@gmail.com>
2021-06-02 09:56:56 -0300

    Importado módulos form-field e input

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -40,6 +42,8 @@ import { CreateScheduleComponent } from './components/create-schedule/create-sch
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatFormFieldModule,
+    MatInputModule, 
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
