52b7dc9397c65e63945a23f8d22cf49ec1b28819
Suelen Almeida <lima.j.diego@gmail.com>
2021-06-02 09:42:03 -0300

    Importado o httpCliente e linkando com o backend pelo metodo post

--- frontend/src/app/components/home-client/schedule.service.ts
+++ frontend/src/app/components/home-client/schedule.service.ts
@@ -1,19 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Cliente } from './../../clientes/shared/cliente';
+import { Client } from './schedule.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewScheduleService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  baseUrl = 'http://localhost:5000/clients'
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMsg(msg: string): void {
     this.snackBar.open(msg, 'X', {
-      duration:2000,
+      duration: 2000,
       horizontalPosition: "right",
       verticalPosition: "top",
-      
     })
   }
+
+  create(schedule: Client): Observable<Cliente> {
+    return this.http.post<Cliente>(this.baseUrl, schedule)
+  }
+
 }
